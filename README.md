27b0b25b30764fda84510c6e22ecd95dc035a216
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2024-11-05 21:18:42 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # ign-basque-benchmark
 Using OSM road data for the IGN Basque road selection benchmark
+
+## Overview
+
+IGN has made a benchmark dataset for road selection methods using data from the Basque region, see https://ica-abs.copernicus.org/articles/4/5/2022/ica-abs-4-5-2022.pdf.
+
+The IGN benchmark shapefiles use probably IGN network data. The goal here is to use OpenStreetMap data instead for the same benchmark.
+
+<img src="screenshot.png">
+
+<i>Roads from OpenStreetMap with `highway=*` in light color and the ones that roughly match the IGN benchmark final shapefile in dark color.</i>
+
+## Steps
+
+1. Buffer IGN final roads in QGIS with 5 m.
+1. Load OSM extract.
+1. Intersect OSM with IGN buffered poylgons.
+1. Save as "clipped-osm.gpkg".
+1. Run `python3 select.py`.
+
+## License
+
+- OpenStreetMap files as ODbL https://www.openstreetmap.org/copyright
+  - filtered_output.gpkg
+  - osm.gpkg
+  - osm-clipped.gpkg
+- IGN shapefiles as CC-BY https://zenodo.org/records/5744120
+- Code as MIT
+
---
+++ select.py
@@ -0,0 +1,48 @@
+import geopandas as gpd
+from shapely.geometry import MultiLineString
+import matplotlib.pyplot as plt
+
+gdf = gpd.read_file("osm-clipped.gpkg", layer="osm")
+
+clipped_lengths = {}
+for idx, row in gdf.iterrows():
+    geometry = row.geometry
+    if isinstance(geometry, MultiLineString):
+        total_length = sum(line.length for line in geometry.geoms)  # Sum the lengths of LineStrings
+        # print(f"osm_id: {row['osm_id']}, Total Length of Lines: {total_length}")
+        clipped_lengths[row['osm_id']] = total_length
+    else:
+        print(f"osm_id: {row['osm_id']} has a different geometry type: {type(geometry)}")
+
+
+gdf = gpd.read_file("osm.gpkg", layer="osm")
+
+lengths = {}
+for idx, row in gdf.iterrows():
+    geometry = row.geometry
+    # print(geometry.length)
+    lengths[row['osm_id']] = geometry.length
+
+# # Plot the histogram
+# plt.hist(lengths.values(), bins=10, range=(0.0, 0.001), edgecolor='black')
+
+# # Labeling the plot
+# plt.xlabel('lengths')
+# plt.ylabel('Frequency')
+# plt.title('Histogram of Ratios')
+
+# # Display the histogram
+# plt.show()
+
+keep = []
+min_length = 0.0005
+for clipped_key in clipped_lengths:
+    ratio = clipped_lengths[clipped_key] / lengths[clipped_key]
+    if ratio > 0.2 and lengths[clipped_key] > min_length:
+        keep.append(clipped_key)
+
+# Filter the GeoDataFrame to include only features with matching osm_ids
+filtered_gdf = gdf[gdf['osm_id'].isin(keep)]
+
+# Save the filtered GeoDataFrame to a new GeoPackage file
+filtered_gdf.to_file("filtered_output.gpkg", layer="osm", driver="GPKG")
